f2106e9eb85c4c589ac09581c9cb2a02c50ef3f2
Peter deHaan <pdehaan@mozilla.com>
2013-10-29 13:07:35 -0700

    Adding a copyright task to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,15 +89,43 @@ module.exports = function (grunt) {
       options: {
         jshintrc: ".jshintrc"
       }
+    },
+    copyright: {
+      files: [
+        "**/*.js",
+        "!**/node_modules/**"
+      ],
+      options: {
+        pattern: "This Source Code Form is subject to the terms of the Mozilla Public"
+      }
     }
   });
 
   require('load-grunt-tasks')(grunt);
 
+  grunt.registerMultiTask('copyright', 'Copyright all the things!', function () {
+    var pattern = this.options().pattern;
+    var files = this.filesSrc.map(function (file) {
+      return {
+        "name": file,
+        "txt": grunt.file.read(file, "utf8")
+      };
+    }).filter(function (file) {
+      return !file.txt.match(pattern);
+    });
+
+    if (files.length) {
+      grunt.log.subhead("The following files are missing copyright headers:");
+      files.forEach(function (file) {
+        grunt.log.warn(file.name);
+      });
+      return false;
+    }
+  });
+
   grunt.registerTask('default', ['browserify:basic']);
   grunt.registerTask('dev', ['watch']);
   grunt.registerTask('benchmark', ['browserify:benchmark']);
   grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('qa', ['copyright', 'jshint']);
 };
-
-
