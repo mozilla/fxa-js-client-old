cda17c7192e043b7a9a56bafe1ed7056355cc111
Zachary Carter <zack.carter@gmail.com>
2013-12-18 10:57:31 -0800

    update browser request
    Conflicts:
    	web/example.html

--- web/bundle.js
+++ web/bundle.js
@@ -3500,7 +3500,7 @@ function run_xhr(options) {
     xhr.headers = xhr.getAllResponseHeaders().split('\r\n').reduce(function(headers, header) {
       var pair = header.split(": ");
       if (pair[0]) {
-        headers[pair[0]] = pair[1];
+        headers[pair[0].toLowerCase()] = pair[1];
       }
       return headers;
     }, {});
